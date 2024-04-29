b18d2a2370e4569b3369860e984e8c378f54e5da
Ferrán Prado Roesner <77746263+FerranPradoRo@users.noreply.github.com>
2024-04-28 19:42:01 -0600

    Add files via upload

---
+++ DataPathVerilog/BancoRegistro.v
@@ -0,0 +1,26 @@
+// 1. Declarar el modulo y sus I/O
+module BancoDeRegistro(
+	input [4:0] RA1,
+	input [4:0] RA2,
+	input [31:0]Di,
+	input [4:0] Dir,
+	input RegWrite,
+	output reg [31:0] DR1,
+	output reg [31:0] DR2 	
+);
+//2. Definir Elementos /Componentes Internos (Cables, Registros).
+reg [31:0] MEM [0:31];
+
+// 3. Elementos de Procesamiento (Asignacion de Operadores "Logicos y Aritmeticos")
+
+always @*
+	begin
+		if(RegWrite)
+		begin
+			MEM[Dir]=Di;
+		end
+		DR1=MEM[RA1];
+		DR2=MEM[RA2];
+	end
+
+endmodule
