a7a51819ba3af5662b796a068f79577d83e0d264
Ferrán Prado <ferranprador@gmail.com>
2024-05-13 17:50:02 -0600

    Multiplexores actualizados

--- DataPathVerilog/Mux2_1_32.v
+++ DataPathVerilog/Mux2_1_32.v
@@ -1,13 +1,10 @@
-// 1. Declarar el modulo y sus I/O
 module Mux2_1_32(
 	input  sel,
 	input [31:0] Op1,
 	input [31:0] Op2,
 	output reg [31:0]outOp
 );
-//2. Definir Elementos /Componentes Internos (Cables, Registros).
 
-//3. Elementos de Procesamiento (Lógicos y aritméticos)
 always @ (*)
 	begin 
 		if(sel)
--- DataPathVerilog/Mux2_1_8.v
+++ DataPathVerilog/Mux2_1_8.v
@@ -1,13 +1,10 @@
-// 1. Declarar el modulo y sus I/O
 module Mux2_1_8(
 	input  sel,
 	input [7:0] Op1,
 	input [7:0] Op2,
 	output reg [7:0]outOp
 );
-//2. Definir Elementos /Componentes Internos (Cables, Registros).
 
-//3. Elementos de Procesamiento (Lógicos y aritméticos)
 always @ (*)
 	begin 
 		if(sel)
