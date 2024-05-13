f640ca874189015c38b3e3afb625bbee3ed45a95
AlanC54 <167790981+AlanC54@users.noreply.github.com>
2024-05-13 10:48:38 -0600

    Update ALU_suma.v

--- DataPathVerilog/ALU_suma.v
+++ DataPathVerilog/ALU_suma.v
@@ -1,11 +1,10 @@
 module ADD(
     input [31:0] operando1,    // Primer operando de 32 bits
-    input [31:0] operando2,    // Segundo operando de 32 bits
-    output reg [31:0] resultado  // Resultado de la operación ADD
+    output reg [31:0] resultado  // Resultado de la operaciÃ³n ADD
 );
 
     always @(*) begin
-        resultado = operando1 + operando2;  // Realiza la operación de suma ADD
+        resultado = operando1 + 0b'4;  // Realiza la operaciÃ³n de suma ADD
     end
 
 endmodule
