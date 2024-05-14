4350fd5c4d0e9bdf67396249f3b683cc02bf7ae3
Ferrán Prado <ferranprador@gmail.com>
2024-05-13 18:16:54 -0600

    Módulo SignExtend creado

---
+++ DataPathVerilog/SignExtend.v
@@ -0,0 +1,13 @@
+module Mux2_1_5(
+	input [15:0] in,
+	output [31:0] out
+);
+
+always @ (*)
+	begin 
+		if(in[15] == 1'b1)
+			out = {16'b1111111111111111, in};
+		else
+			out = {16'b0, in};
+	end
+endmodule
